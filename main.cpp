63468b404b1f79b0495e97daaaf90e5cb3024ac9
Timothy Graham <timothygrahamm@gmail.com>
2020-07-14 11:06:36 +1000

    make it follow each dot

--- main.cpp
+++ main.cpp
@@ -5,14 +5,42 @@
 #include <ctime>
 #include "test.hpp"
 
+std::vector<sf::Vector2f> GeneratePath(sf::Vector2f origin, sf::Vector2f goal){
+    std::cout << "entering generate path" << std::endl;
+    bool goal_reached = false;
+    std::vector<sf::Vector2f> path = std::vector<sf::Vector2f>();
+    while (!goal_reached){
+
+        if (origin.x < goal.x && std::abs(origin.x - goal.x) >= 20){
+            origin.x += 10;
+        }
+        else if (origin.x > goal.x && std::abs(origin.x - goal.x) >= 20){
+            origin.x -= 10;
+        }
+
+        if (origin.y < goal.y && std::abs(origin.y - goal.y) >= 20){
+            origin.y += 10;
+        }
+        else if (origin.y > goal.y && std::abs(origin.y - goal.y) >= 20){
+            origin.y -= 10;
+        }
+
+        path.push_back(sf::Vector2f(origin.x, origin.y));
+        if (std::abs(origin.x - goal.x) <= 20 && std::abs(origin.y - goal.y) <= 20){
+            goal_reached = true;
+        }
+    }
+    std::cout << "leaving generate path" << std::endl;
+    return path;
+}
+
 int WinMain()
 {
-    sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
-    sf::CircleShape order(10.f);
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "pathfinding");
+    sf::CircleShape order(5.f);
     sf::Vector2f order_pos(300.f,600.f);
     order.setPosition(order_pos);
-
-    sf::CircleShape shape(50.f);
+    sf::CircleShape shape(5.f);
     sf::Vector2f shape_pos(100.f,100.f);
     shape.setPosition(shape_pos);
 
@@ -27,15 +55,19 @@ int WinMain()
     fps.setPosition(1500.f, 800.f);
     fps.setFillColor(sf::Color::White);
     fps.setString("0");
+    
 
     shape.setFillColor(sf::Color::Blue);
     order.setFillColor(sf::Color::Green);
     
     std::clock_t start;
+    bool path_shapes_set = false;
     double duration;
-
+    std::vector<sf::Vector2f> path;
+    std::vector<sf::CircleShape> path_shapes;
     start = std::clock();
     int frames = 0;
+    int path_step = 0;
     while (window.isOpen())
     {
         sf::Event event;
@@ -46,30 +78,53 @@ int WinMain()
             if (event.type == sf::Event::Closed)
                 window.close();
             
-             if (event.type == sf::Event::MouseButtonPressed){
-                 order_pos.x = event.mouseButton.x;
-                 order_pos.y = event.mouseButton.y;
-                 order.setPosition(order_pos);
+            if (event.type == sf::Event::MouseButtonPressed){
+                 
+                order_pos.x = event.mouseButton.x;
+                order_pos.y = event.mouseButton.y;
+                order.setPosition(order_pos);
+                path = GeneratePath(shape_pos, order_pos);
+                int i = 0;
+                path_shapes = std::vector<sf::CircleShape>();
+                std::cout << "cleared shapes" << std::endl;
+                for(sf::Vector2f point : path){
+                    std::cout << point.x << std::endl;
+                    sf::CircleShape temp_path_shape = sf::CircleShape(1.f);
+                    temp_path_shape.setPosition(point.x,point.y);
+                    temp_path_shape.setFillColor(sf::Color::White);
+                    path_shapes.push_back(temp_path_shape);
+                }
+                path_shapes_set = true; 
              }
         }
 
-        if (shape_pos.x < order_pos.x){
-            shape_pos.x += 0.01;
-        }
-        else{
-            shape_pos.x -= 0.01;
-        }
-        if (shape_pos.y < order_pos.y){
-            shape_pos.y += 0.01;
-        }
-        else{
-            shape_pos.y -= 0.01;
+        if (path_step < path.size()){
+            if (shape_pos.x < path.at(path_step).x){
+                shape_pos.x += 0.01;
+            }
+            else{
+                shape_pos.x -= 0.01;
+            }
+            if (shape_pos.y <  path.at(path_step).y){
+                shape_pos.y += 0.01;
+            }
+            else{
+                shape_pos.y -= 0.01;
+            }
+            if (std::abs(shape_pos.x -  path.at(path_step).x) <= 20 && std::abs(shape_pos.y -  path.at(path_step).y) <= 20){
+                path_step++;
+            }
         }
 
-        
         shape.setPosition(shape_pos);
+
         duration += ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
         window.clear();
+
+        for(sf::CircleShape point : path_shapes){
+            window.draw(point);
+        } 
+
         window.draw(order);
         window.draw(shape);
         window.draw(fps);
@@ -89,4 +144,6 @@ int WinMain()
     }
 
     return 0;
+
 }
+
